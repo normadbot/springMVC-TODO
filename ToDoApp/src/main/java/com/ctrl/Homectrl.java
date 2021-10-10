8caad894744eda5269258dac1bb27aca27202baf
NormadBot <37356059+normadbot@users.noreply.github.com>
2021-10-10 20:56:11 +0530

    Database Connectivity Code for MySQL WorkBench

--- ToDoApp/src/main/java/com/ctrl/Homectrl.java
+++ ToDoApp/src/main/java/com/ctrl/Homectrl.java
@@ -3,8 +3,6 @@ package com.ctrl;
 import java.util.Date;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,22 +10,23 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.doa.TodoDao;
 import com.entities.Todo;
 
 @Controller
 public class Homectrl {
-	
+
 	@Autowired
-	ServletContext context;
+	TodoDao todoDao;
 
 	@RequestMapping("/home")
 	public String home(Model m) {
 
 		String str = "home";
 		m.addAttribute("page", str);
-		List<Todo> list = (List<Todo>) context.getAttribute("list");
-		
-		m.addAttribute("todos",list);
+		List<Todo> list = this.todoDao.getAll();
+
+		m.addAttribute("todos", list);
 		return "home";
 
 	}
@@ -44,11 +43,9 @@ public class Homectrl {
 
 	@RequestMapping(value = "/saveTodo", method = RequestMethod.POST)
 	public String saveTodo(@ModelAttribute("todo") Todo t, Model m) {
-		List<Todo> list = (List<Todo>) context.getAttribute("list");
-
 		t.setTodoDate(new Date());
-		list.add(t);
-		m.addAttribute("msg","successfully added..");
+		this.todoDao.save(t);
+		m.addAttribute("msg", "successfully added..");
 		System.out.println(t);
 		return "home";
 	}
--- ToDoApp/src/main/java/com/entities/Todo.java
+++ ToDoApp/src/main/java/com/entities/Todo.java
@@ -2,7 +2,16 @@ package com.entities;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int todoId;
 	private String todoTitle;
 	private String todocontent;
 	private Date todoDate;
